4bf3214583139eeb0ed6b1b9774f4d4c95740b2f
Jo Roe <jroe@invoca.com>
2022-01-07 17:07:18 -0800

    all tests working for Day 6 pt. 1

--- lib/day6.rb
+++ lib/day6.rb
@@ -3,6 +3,30 @@
 class Day6
   def self.load_data(file)
     raw = File.read(file)
-    raw.split(',').map(&:strip)
+    raw.split(',').map(&:to_i)
+  end
+
+  def self.add_days(fish_arr, days)
+    days.times {fish_arr = age_fish(fish_arr)}
+    fish_arr
+  end
+
+  def self.age_fish(fish_arr)
+    new_fish = []
+    baby_fish = 0
+
+    fish_arr.each do |fish|
+      if fish > 0
+        new_fish << fish - 1
+      elsif fish == 0
+        new_fish << 6
+        baby_fish += 1
+      end
+    end
+
+    baby_fish.times { new_fish << 8 }
+    new_fish
   end
 end
+
+puts Day6.load_data('data/JoDay6.txt')
--- spec/day6_spec.rb
+++ spec/day6_spec.rb
@@ -3,8 +3,26 @@ require_relative '../lib/day6'
 
 describe 'Day6' do
   fish_arr = Day6.load_data('data/day6example.txt')
-  
+  fish18 = Day6.add_days(fish_arr, 18)
+  fish80 = Day6.add_days(fish_arr, 80)
+
   it 'data load results in 5 initial fish' do
     expect(fish_arr.size).to eq(5)
+  end
+
+  it 'day 1 countdowns = [2,3,2,0,1]' do
+    expect(Day6.add_days(fish_arr, 1)).to eq([2,3,2,0,1])
+  end
+
+  it 'example at 18 days' do
+    expect(fish18).to eq([6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8])
+  end
+
+  it 'at 18 days, 26 fish' do
+    expect(fish18.size).to eq(26)
+  end
+
+  it 'at 80 days, 5934 fish' do
+    expect(fish80.size).to eq(5934)
   end
 end
